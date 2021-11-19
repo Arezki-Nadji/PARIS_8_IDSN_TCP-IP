7078332fa1bf02e83683515655136a1960f4a14e
Arezki-Nadji <arezki.m.nadji96@gmail.com>
2021-11-19 01:37:53 +0100

    This is the Client

--- tcpClient.c
+++ tcpClient.c
@@ -9,6 +9,7 @@
 #define PORT 4455
 
 void main(){
+
 	int clientSocket;
 	struct sockaddr_in serverAddr;
 	char buffer[1024];
@@ -18,10 +19,11 @@ void main(){
 	memset(&serverAddr,'\0',sizeof(serverAddr));
 	serverAddr.sin_family=AF_INET;
 	serverAddr.sin_port=htons(PORT);
-	serverAddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+	serverAddr.sin_addr.s_addr=inet_addr("192.168.1.71");
 	
 	connect(clientSocket,(struct sockaddr*)&serverAddr,sizeof(serverAddr));
 	printf("{+}Connected To the Server\n");
 	recv(clientSocket,buffer,1024,0);
 	printf("{+}Data Received : %s", buffer);
-	}
+	
+}
